a8c52909e819aa97f4b2fa47287996835141371c
xianggp <pengqiqiai@sina.com>
2013-12-17 17:16:58 +0800

    Update DeviceInfo.js

--- www/DeviceInfo.js
+++ www/DeviceInfo.js
@@ -1,32 +1,35 @@
-(function( cordova ) {
+cordova.define("cordova/plugin/DeviceInfo",
+  function(require, exports, module) {
+    var exec = require("cordova/exec");
 
     function DeviceInfo() {}
 
-    DeviceInfo.prototype.imeiNumber = function(win, fail) {
-        return cordova.exec(
-                function (args) { if(win !== undefined) { win(args); } },
-                function (args) { if(fail !== undefined) { fail(args); } },
-                "DeviceInfo", "imeiNumber", []);
-    };
-    DeviceInfo.prototype.phoneNumber = function(win, fail) {
-        return cordova.exec(
-                function (args) { if(win !== undefined) { win(args); } },
-                function (args) { if(fail !== undefined) { fail(args); } },
-                "DeviceInfo", "phoneNumber", []);
-    };
-    DeviceInfo.prototype.phoneSim = function(win, fail) {
-        return cordova.exec(
-                function (args) { if(win !== undefined) { win(args); } },
-                function (args) { if(fail !== undefined) { fail(args); } },
-                "DeviceInfo", "phoneSim", []);
-    };
-
-    if(!window.plugins) {
-        window.plugins = {};
+        DeviceInfo.prototype.imeiNumber = function(win, fail) {
+            return cordova.exec(
+                    function (args) { if(win !== undefined) { win(args); } },
+                    function (args) { if(fail !== undefined) { fail(args); } },
+                    "DeviceInfo", "imeiNumber", []);
+        };
+        DeviceInfo.prototype.phoneNumber = function(win, fail) {
+            return cordova.exec(
+                    function (args) { if(win !== undefined) { win(args); } },
+                    function (args) { if(fail !== undefined) { fail(args); } },
+                    "DeviceInfo", "phoneNumber", []);
+        };
+        DeviceInfo.prototype.phoneSim = function(win, fail) {
+            return cordova.exec(
+                    function (args) { if(win !== undefined) { win(args); } },
+                    function (args) { if(fail !== undefined) { fail(args); } },
+                    "DeviceInfo", "phoneSim", []);
+        };
+  });
+  
+  if(!window.plugins) {
+    window.plugins = {};
     }
-
     if (!window.plugins.DeviceInfo) {
-        window.plugins.DeviceInfo = new DeviceInfo();
+        window.plugins.DeviceInfo = cordova.require("cordova/plugin/DeviceInfo");
     }
 
-})( window.cordova );
+
+
